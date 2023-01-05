65a03e3d983ae05fa08875e241618ee32acc68d0
Thomas <fitzgerald.thomas.e@gmail.com>
2023-01-05 18:48:04 -0500

    methods

--- flicks.rb
+++ flicks.rb
@@ -1,9 +1,14 @@
-puts "Mikey loves Goonies"
-puts "Mikey's favorite movie is Goonies"
+def weekday
+    current_time = Time.new
+    current_time.strftime("%A")
+end
 
-movie = 'Goonies'
-puts "Mikey's favorite movie is #{movie}"
+def movie_listing(title, rank=0)
+    "#{weekday.upcase}: #{title.capitalize} has a rank of #{rank}."
+end
 
-rank = 10
+ puts movie_listing("goonies", 10)
+ puts movie_listing("ghostbusters", 9)
 
-puts "#{movie} has a rank of #{rank * 2}"
+ a_title = "goldfinger"
+ puts movie_listing(a_title)
